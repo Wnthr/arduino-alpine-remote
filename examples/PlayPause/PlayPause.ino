5604faeff6f1b57c9715459f2a2c871b6d2d6b41
Mattias Winther <mattias.winther@gmail.com>
2016-08-02 21:24:53 +0200

    Closed #2, closed #5, closed #6: Docs update

---
+++ examples/PlayPause/PlayPause.ino
@@ -0,0 +1,34 @@
+/*
+  PlayPause
+
+ Alternates between sending play and pause to the Alpine head unit
+ every three seconds.
+
+
+ The circuit:
+ 3.5mm phono jack tip connected to pin 3 on the Arduino.
+
+ created 2016
+ by Mattias Winther <mattias.winther@gmail.com>
+
+ This example code is based on the Button example for the Arduino and is released
+ in the public domain.
+
+ */
+
+#include "AlpineRemote.h"
+
+// constants won't change. They're used here to
+// set pin numbers:
+const int alpinePin =  3;      // the number of the Alpine +5V pin (the tip of the 3.5mm plug)
+
+// initialize an AlpineRemote instance:
+AlpineRemote alpine(alpinePin);
+
+void setup() {
+}
+
+void loop() {
+  alpine.sendPlay();
+  delay(3000);
+}
